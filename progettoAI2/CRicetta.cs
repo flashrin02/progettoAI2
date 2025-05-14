49925a9207b1d5659087198dade4c8394dffe111
flashrin02 <154712138+flashrin02@users.noreply.github.com>
2025-05-14 14:50:38 +0200

    a scuola 1
    crecipe

---
+++ progettoAI2/CRicetta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progettoAI2
+{
+    internal class CRicetta
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> directions { get; set; }
+        public string link { get; set; }
+
+        public string source { get; set; }
+
+        public string ner { get; set; }
+        public CRicetta(int ID, string nome, List<string> ingredienti, List<string> istruzioni, string link, string source, string ner)
+        {
+            this.ID = ID;
+            title = nome;
+            Ingredients = ingredienti;
+            directions = istruzioni;
+            this.link = link;
+            this.source = source;
+            this.ner = ner;
+        }
+        public void StampaRicetta()
+        {
+            Console.WriteLine($"Nome: {title}");
+            Console.WriteLine("Ingredienti:");
+            foreach (var ingrediente in Ingredients)
+            {
+                Console.WriteLine($"- {ingrediente}");
+            }
+            Console.WriteLine("Istruzioni:");
+            foreach (var istruzione in directions)
+            {
+                Console.WriteLine($"- {istruzione}");
+            }
+            Console.WriteLine($"Link: {link}");
+            Console.WriteLine($"Source: {source}");
+        }
+    }
+}
--- progettoAI2/Program.cs
+++ progettoAI2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using progettoAI2;
+
+string path = "full_dataset.csv";
+List<CRicetta> ricette = new List<CRicetta>();
+if (File.Exists(path))
+{
+    using (StreamReader sr = new StreamReader(path))
+    {
+        string line;
+        bool isHeader = true;       //Per saltare la prima riga di intestazione
+        while ((line = sr.ReadLine()) != null)
+        {
+            if (isHeader)
+            {
+                isHeader = false;
+                continue;
+            }
+
+            string[] parts = line.Split(',');
+
+            int ID = int.Parse(parts[0]);
+            string title = parts[1];
+            List<string> ingredients = parts[2].Split(',').ToList();
+            List<string> directions = parts[2].Split(',').ToList();
+            string link = parts[4];
+            string source = parts[5];
+            string ner = parts[6];
+            CRicetta ricetta = new CRicetta(ID, title, ingredients, directions, link, source, ner);
+            ricette.Add(ricetta);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("File non trovato.");
+}
